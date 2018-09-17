497732689c1a9eb824d2a64e21a3642051108d6a
WATANABEK <38969200+WataKN@users.noreply.github.com>
2018-09-18 00:21:43 +0900

    Add files via upload

---
+++ 201809_TEST2/animation.php
@@ -0,0 +1,59 @@
+
+<html style='max-width: 800px;'>
+<head>
+<script type="text/javascript">
+  document.onkeydown = KeyCode;
+  x= 300;
+  y= 200;
+
+function KeyCode(){
+	if(document.all){
+		kcode = event.keyCode;
+	}else if(document.getElementById){
+        kcode = (event.keyCode)? event.keyCode: event.charCode;
+	}else if(document.layers){
+		kcode = event.which;
+	}
+
+	var src = document.Mario.src;
+
+	switch(kcode){
+		case 0x25:
+		// 「←」キー
+			if (x>1 && x!=0){
+              	x-= 2;
+				if(src.match('imgs/mario_4.png')){
+					document.Mario.src = 'imgs/mario_5.png';
+				}else if(src.match('imgs/mario_5.png')){
+					document.Mario.src = 'imgs/mario_6.png';
+				}else{
+					document.Mario.src = 'imgs/mario_4.png';
+				}
+			}
+		break;
+		case 0x27:
+    	// 「→」キー
+			if(x<600){
+				x+= 2;
+				if(src.match('imgs/mario_1.png')){
+					document.Mario.src = 'imgs/mario_2.png';
+				}else if(src.match('imgs/mario_2.png')){
+					document.Mario.src = 'imgs/mario_3.png';
+				}else{
+					document.Mario.src = 'imgs/mario_1.png';
+				}
+			}
+		break;
+	}
+}
+  function moveImage(){
+    document.Mario.style.left= x;
+  }
+</script>
+</head>
+<body bgcolor="#f4f8ff" onLoad="setInterval('moveImage()',25)">
+
+<h2>TEST2 ANIMATION</h2>
+<img src="imgs/mario_1.png" name="Mario" id='Mario' style="position:absolute;left:300px;top:200px;">
+</body>
+</html>
